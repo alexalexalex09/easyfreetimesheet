aa129c6684163b0a55817877a1d53c40a7a84957
Alex <frogitts@gmail.com>
2022-08-05 21:51:53 -0400

    Adding backend and animations

---
+++ routes/auth.js
@@ -0,0 +1,37 @@
+require("dotenv").config();
+var passport = require("passport");
+const express = require("express");
+const router = express.Router();
+
+router.get(
+  "/login",
+  passport.authenticate("google", { scope: ["profile", "email", "openid"] })
+);
+
+router.get(
+  "/auth/google/callback",
+  (req, res, next) => {
+    next();
+  },
+  passport.authenticate("google", {
+    failureRedirect: process.env.CLIENT_URL + "?err=cannot_login",
+    session: false,
+  }),
+  function (req, res) {
+    req.logIn(req.user, function (err) {
+      res.redirect(process.env.CLIENT_URL + "/");
+    });
+  }
+);
+
+router.get("/logout", (req, res) => {
+  console.log("Logout");
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+module.exports = router;
