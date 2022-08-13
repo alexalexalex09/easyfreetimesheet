2b96d595cbdb68d2711c96cfb6a5522b4f991ad0
Alex <frogitts@gmail.com>
2022-08-13 10:14:21 -0400

    removed secure cookies

--- app.js
+++ app.js
@@ -36,15 +36,15 @@ var sess = {
   collectionName: "mongoSessions",
   cookie: {},
 };
-/*if (app.get("env") === "production") {
+if (app.get("env") === "production") {
   // Use secure cookies in production (requires SSL/TLS)
-  sess.cookie.secure = true;
+  //sess.cookie.secure = true;
 
   // Uncomment the line below if your application is behind a proxy (like on Heroku)
   // or if you're encountering the error message:
   // "Unable to verify authorization request state"
   app.set("trust proxy", 1);
-}*/
+}
 app.use(session(sess));
 
 //Auth
